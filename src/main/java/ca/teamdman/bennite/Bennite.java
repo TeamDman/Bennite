5a10dae1eb553300b59d9c29cfcd0fb993b2df54
TeamDman <TeamDman9201@gmail.com>
2020-02-24 18:48:57 -0500

    Basic mod configuration

--- src/main/java/ca/teamdman/bennite/Bennite.java
+++ src/main/java/ca/teamdman/bennite/Bennite.java
@@ -1,8 +1,8 @@
-package net.fabricmc.example;
+package ca.teamdman.bennite;
 
 import net.fabricmc.api.ModInitializer;
 
-public class ExampleMod implements ModInitializer {
+public class Bennite implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
