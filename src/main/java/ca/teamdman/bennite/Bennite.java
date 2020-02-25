2d0f2d39d2f26ae03062216e3031e4020a7479ed
TeamDman <TeamDman9201@gmail.com>
2020-02-24 20:05:02 -0500

    Remove dead mixins, add burn time

--- src/main/java/ca/teamdman/bennite/Bennite.java
+++ src/main/java/ca/teamdman/bennite/Bennite.java
@@ -3,11 +3,9 @@ package ca.teamdman.bennite;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.block.FabricBlockSettings;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.fabricmc.fabric.api.registry.FuelRegistry;
 import net.minecraft.block.Material;
-import net.minecraft.item.BlockItem;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemStack;
+import net.minecraft.item.*;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
@@ -22,11 +20,12 @@ public class Bennite implements ModInitializer {
 	public static final ItemGroup    BENNITE_GROUP      = FabricItemGroupBuilder.build(
 			new Identifier(MODID, MODID),
 			() -> new ItemStack(Bennite.BENNITE_BLOCK_ITEM));
-	public static final BlockItem    BENNITE_BLOCK_ITEM = new BlockItem(BENNITE_BLOCK, new Item.Settings().group(BENNITE_GROUP));
+	public static final BlockItem    BENNITE_BLOCK_ITEM = new BlockItem(BENNITE_BLOCK,new Item.Settings().group(BENNITE_GROUP));
 
 	@Override
 	public void onInitialize() {
 		Registry.register(Registry.BLOCK, new Identifier(MODID, "bennite"), BENNITE_BLOCK);
 		Registry.register(Registry.ITEM, new Identifier(MODID, "bennite"), BENNITE_BLOCK_ITEM);
+		FuelRegistry.INSTANCE.add(BENNITE_BLOCK_ITEM, FuelRegistry.INSTANCE.get(Items.LAVA_BUCKET)*20);
 	}
 }
